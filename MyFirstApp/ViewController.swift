0c760e05acdff0d4e71b60d3e50bfad192034737
grade <grade@lab122-11.local>
2018-10-11 19:48:07 +0300

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by grade on 11.10.18.
+//  Copyright © 2018 grade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
